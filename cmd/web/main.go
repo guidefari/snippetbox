878fd407329f1e7943018b20bd39e3a1eecc8b6b
Guide Fari <guide.fari@flexperto.com>
2023-09-01 06:41:57 +0200

    refactor: server config

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -20,7 +20,13 @@ func main() {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
+	server := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
+
 	infoLog.Printf("Starting server on %s", *addr)
-	err := http.ListenAndServe(*addr, mux)
+	err := server.ListenAndServe()
 	errorLog.Fatal(err)
 }
